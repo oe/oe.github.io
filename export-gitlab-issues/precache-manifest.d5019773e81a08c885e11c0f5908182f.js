f4550db0b1517acd97ef5ad47c047f168bd3c0ce
Saiya <okarl@outlook.com>
2019-07-05 11:41:59 +0800

    added export gitlab issues

---
+++ export-gitlab-issues/precache-manifest.d5019773e81a08c885e11c0f5908182f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecbb00ce9d9937e2fec3",
+    "url": "./static/css/main.61de3c49.chunk.css"
+  },
+  {
+    "revision": "ecbb00ce9d9937e2fec3",
+    "url": "./static/js/main.ecbb00ce.chunk.js"
+  },
+  {
+    "revision": "bbe73f7a37bc8c858baa",
+    "url": "./static/css/1.0c305d6c.chunk.css"
+  },
+  {
+    "revision": "bbe73f7a37bc8c858baa",
+    "url": "./static/js/1.bbe73f7a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6ac7497ba5f6a010eb2a094237bf718c",
+    "url": "./index.html"
+  }
+];
